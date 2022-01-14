bf9285c0f2e8bc34201a7d7469252f3d852d615f
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-14 16:40:56 -0300

    req 1

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const talker = (req, res) => {
+  const dataTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+  res.status(200).send(dataTalker);
+};
+
+app.get('/talker', talker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
