66c0b07b8d46e55b39f303bbc39c04aba58af4fc
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-14 17:11:41 -0300

    req 2

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const talker = (req, res) => {
+const talkers = (req, res) => {
   const dataTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
   res.status(200).send(dataTalker);
 };
 
-app.get('/talker', talker);
+app.get('/talker', talkers);
+
+const findTalker = (req, res) => {
+  const { id } = req.params;
+  const dataTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+  const talker = dataTalker.find((talkerId) => talkerId.id === Number(id));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(HTTP_OK_STATUS).send(talker);
+};
+
+
+
+app.get('/talker/:id', findTalker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
