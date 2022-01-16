37727a70537bd8129e8188e42de1f4d17568d8fd
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-16 17:52:16 -0300

    endireita middleware do login, req 3

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,3 +1,28 @@
-const login = (req, res) => res.status(200).json({ token: '7mqaVRXJSp886CGr' });
+const crypto = require('crypto');
 
+// Denis Espanhol
+// crypto.randomBytes(16).toString('hex').slice(0, 16)
+
+// Michael Caxias
+const isEmailValid = (email) => {
+  const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regexEmail.test(email) === true;
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!isEmailValid(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return res.status(200).json({ token: crypto.randomBytes(16).toString('hex').slice(0, 16) });
+};
 module.exports = login;
